9bdbfdce22cdaa357da998f9b760ca260b89be6e
Renato de Pierri <renatopierri@users.noreply.github.com>
2025-10-16 18:39:50 +0000

    Mova o arquivo App.test.jsx para dentro da pasta pages e acertando o caminho do import

--- src/tests/pages/App.test.jsx
+++ src/tests/pages/App.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../pages/index";
+import Home from "../../pages/index";
 
 describe("Página inicial", () => {
   it("deve renderizar o título principal", () => {
