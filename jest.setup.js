2f839007fda66dd255f77f5f28106ee755551fee
Renato de Pierri <renatopierri@users.noreply.github.com>
2025-10-16 18:23:43 +0000

    ajustes de acordo com o tutorial

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,15 @@
 //import "@testing-library/jest-dom";
+// jest.setup.js
 require("@testing-library/jest-dom");
+
+// ✅ Silencia o warning do React sobre JSX transform antigo
+const originalWarn = console.warn;
+console.warn = (...args) => {
+  if (
+    typeof args[0] === "string" &&
+    args[0].includes("using an outdated JSX transform")
+  ) {
+    return;
+  }
+  originalWarn(...args);
+};
