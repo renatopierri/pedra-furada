6406f5c46da7b439cbbbfd3c8beb4254d00c3ff1
Renato de Pierri <renatopierri@users.noreply.github.com>
2025-10-21 23:36:03 +0000

    Refactor: Refatoração do Layout.jsx, Header.jsx e criação do componente Footer.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="layout">
       <Header />
-      <main style={{ padding: "1rem" }}>{children}</main>
-      <footer>
-        <small>© 2025 Pousada Pedra Furada</small>
-      </footer>
+      <main style={{ padding: "1rem" }}>
+        {children}
+      </main>
+      <Footer />
     </div>
   );
 }
