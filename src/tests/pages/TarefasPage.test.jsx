61998e2d27e812040ba8c7616d379db5f031f35e
Renato de Pierri <renatopierri@users.noreply.github.com>
2025-10-22 21:10:04 +0000

    test: Configurando TarefasPage.test.jsx para testar de maneira deterministica

--- src/tests/pages/TarefasPage.test.jsx
+++ src/tests/pages/TarefasPage.test.jsx
@@ -4,12 +4,16 @@ import TarefasPage from "../../pages/TarefasPage";
 
 describe("TarefasPage.jsx — Gerenciamento das tarefas diárias", () => {
   test("deve atualizar o status da tarefa ao clicar no botão", () => {
+    // Renderiza o componente
     render(<TarefasPage />);
+
     // Seleciona o primeiro botão "Atualizar"
     const botao = screen.getAllByText("Atualizar")[0];
-    // Simula o clique do usuário
+
+    // Simula o clique do usuário no primeiro item
     fireEvent.click(botao);
-    // Verifica se o status foi alterado para "Em andamento" ou "Concluída"
-    expect(screen.getByText(/Em andamento|Concluída/)).toBeInTheDocument();
+
+    // Verifica se o status do primeiro item mudou para "Em andamento"
+    expect(screen.getByText("Em andamento")).toBeInTheDocument();
   });
 });
