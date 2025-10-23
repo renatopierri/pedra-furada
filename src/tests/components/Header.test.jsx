11e3f55d29d67a7765fd6581403e6d6419ddd10c
Renato de Pierri <renatopierri@users.noreply.github.com>
2025-10-23 19:20:07 +0000

    test: Adicionado o teste para verificar se ocorreu o import do react

--- src/tests/components/Header.test.jsx
+++ src/tests/components/Header.test.jsx
@@ -7,3 +7,18 @@ describe("Header", () => {
     expect(screen.getByText("Pousada Pedra Furada")).toBeInTheDocument();
   });
 });
+
+describe("Verifica se importou o React", () => {
+  it('deve conter "import React from \\"react\\";" no topo do arquivo', () => {
+    // Caminho absoluto para o arquivo index.jsx
+    const filePath = path.resolve(__dirname, "../pages/index.jsx");
+
+    // Lê o conteúdo do arquivo como texto
+    const content = fs.readFileSync(filePath, "utf-8");
+
+    // Remove espaços e quebras de linha do início e verifica se começa com o import
+    expect(content.trimStart().startsWith('import React from "react";')).toBe(
+      true
+    );
+  });
+});
